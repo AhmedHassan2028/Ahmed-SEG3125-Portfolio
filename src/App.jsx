a4da1587aa119dcdf5e4fa1f2ad11abfb03de111
AhmedHassan2028 <ahfa871@gmail.com>
2026-05-22 22:51:52 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import Button from 'react-bootstrap/Button';
+import './App.css'
+import BasicExample from './components/nabvar';
+
+
+function App() {
+  return (
+    <>
+      <BasicExample />
+    </>
+  )
+}
+
+export default App
